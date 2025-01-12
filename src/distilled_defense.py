ddf5d2b46290c1e1a0a79d2c7e9d5966aee5ec17
malinamanolache12 <malinamanolache12@gmail.com>
2025-01-12 18:39:37 +0000

    add function for training defence distillation

--- src/distilled_defense.py
+++ src/distilled_defense.py
@@ -6,6 +6,8 @@ on labels obtained by applying softmax, with different temperatures, to the logi
 
 from networks.vgg10 import VGG10, VGG10_lighter
 from networks.inception_v3 import InceptionV3
+from hyperparameters import *
+from train_loop_baseline import compute_class_weights
 
 import os, torch, random, shutil
 import numpy as np
@@ -15,7 +17,7 @@ from PIL import Image
 
 Image.MAX_IMAGE_PIXELS = None
 from torchvision import transforms as T
-
+from torch.utils.tensorboard import SummaryWriter
 torch.manual_seed(2024)
 from dataloaders import get_dls
 
@@ -31,7 +33,7 @@ def compute_distilled_labels(model, device, pretraiend_path):
         raise ValueError("pretrained_path can't be None")
 
     # hyperparameters
-    temperatures = [1, 5, 10, 20, 40, 50]
+    temperatures = [1, 5, 10, 20]
     batch_size = 1
     num_workers = 1
     root = "dataset/raw-img"
@@ -103,11 +105,138 @@ def compute_distilled_labels(model, device, pretraiend_path):
                     softmax_outputs.insert(0, img_path[0])
                     writer.writerow(softmax_outputs)
 
+def train_distillation_defence(model, device, distillation_path=None):
+    
+    _, filename = os.path.split(distillation_path)
+    temp = filename.split(".")[0].split("_")[-1]
+    
+    print(f"Training distillation model for temperature {temp}")
+    runs_path = os.path.join("src/runs", f"distil_temp_{temp}")
+    model_dir = os.path.join(f"src/checkpoints/models_temp_{temp}")
+    os.makedirs(model_dir, exist_ok=True)
+    
+    writer = SummaryWriter(log_dir=runs_path)
+    tr_dl, val_dl, classes, cls_counts = get_dls(
+        root=dataset_root,
+        train_transformations=train_tfs,
+        val_transformations=val_tfs,
+        batch_size=batch_size,
+        split=[0.8, 0.2],
+        num_workers=num_workers,
+        distilled_labels_path=distillation_path
+    )
+
+    model = model.to(device)
+
+    # Compute class weights
+    class_weights = compute_class_weights(cls_counts).to(device)
+
+    # Loss function with weights
+    criterion = nn.CrossEntropyLoss(weight=class_weights)
+    optimizer = optim.Adam(model.parameters(), lr=lr)
+
+    # Track metrics
+    for epoch in range(epochs):
+        print(f"Epoch {epoch + 1}/{epochs}")
+
+        if epoch == 10:
+            new_lr = 1e-5
+            for param_group in optimizer.param_groups:
+                param_group['lr'] = new_lr
+            print(f"Learning rate updated to {new_lr}")
+
+        # Training loop
+        model.train()
+        train_loss = 0.0
+        correct_train = 0
+        total_train = 0
+        all_preds_train = []
+        all_labels_train = []
+        for batch in tqdm(tr_dl, desc="Training"):
+            optimizer.zero_grad()
+
+            qry_im = batch["qry_im"].to(device)
+            qry_gt = batch["qry_gt"].to(device)
+            distil_label = batch["distilled_label"].to(device)
+
+            outputs = model(qry_im)
+            loss = criterion(outputs, distil_label)
+            loss.backward()
+            optimizer.step()
+
+            train_loss += loss.item()
+            _, preds = torch.max(outputs, 1)
+            correct_train += (preds == qry_gt).sum().item()
+            total_train += qry_gt.size(0)
+
+            # Collect predictions and labels for F1 score
+            all_preds_train.extend(preds.cpu().numpy())
+            all_labels_train.extend(qry_gt.cpu().numpy())
+
+        train_accuracy = correct_train / total_train
+        train_loss = train_loss / len(tr_dl)
+        train_f1_scores = f1_score(all_labels_train, all_preds_train, average=None)
+
+        print(
+            f"Training Loss: {train_loss:.4f}, Training Accuracy: {train_accuracy:.4f}"
+        )
+        for cls_idx, f1 in enumerate(train_f1_scores):
+            print(f"F1 Score for Class {cls_idx}: {f1:.4f}")
+
+        writer.add_scalar("Loss/train", train_loss, epoch)
+        writer.add_scalar("Accuracy/train", train_accuracy, epoch)
+
+        # Validation loop
+        model.eval()
+        val_loss = 0.0
+        correct_val = 0
+        total_val = 0
+        all_preds_val = []
+        all_labels_val = []
+
+        with torch.no_grad():
+            for batch in tqdm(val_dl, desc="Validation"):
+                qry_im = batch["qry_im"].to(device)
+                qry_gt = batch["qry_gt"].to(device)
+
+                outputs = model(qry_im)
+                loss = criterion(outputs, qry_gt)
+
+                val_loss += loss.item()
+                _, preds = torch.max(outputs, 1)
+                correct_val += (preds == qry_gt).sum().item()
+                total_val += qry_gt.size(0)
+
+                # Collect predictions and labels for F1 score
+                all_preds_val.extend(preds.cpu().numpy())
+                all_labels_val.extend(qry_gt.cpu().numpy())
+
+        val_accuracy = correct_val / total_val
+        val_loss = val_loss / len(val_dl)
+        val_f1_scores = f1_score(all_labels_val, all_preds_val, average=None)
+        print(
+            f"Validation Loss: {val_loss:.4f}, Validation Accuracy: {val_accuracy:.4f}"
+        )
+        for cls_idx, f1 in enumerate(val_f1_scores):
+            print(f"F1 Score for Class {cls_idx}: {f1:.4f}")
+
+        writer.add_scalar("Loss/val", val_loss, epoch)
+        writer.add_scalar("Accuracy/val", val_accuracy, epoch)
+
+        model_path = os.path.join(model_dir, f"model_{epoch}.pth")
+
+        torch.save(model.state_dict(), model_path)
+
+    print("Training complete.")
+    writer.flush()
+    writer.close()
+
 
 if __name__ == "__main__":
 
     model = VGG10_lighter(num_classes=10)
-    pretrained_path = "checkpoints/VGG10lightweight_10epchs1e-4_5epochs1e-5.pth"
+    pretrained_path = "/home/ModelRobustnessClassifier/checkpoints/model_14.pth"
     device = "cuda"
 
-    compute_distilled_labels(model, device, pretrained_path)
+    # compute_distilled_labels(model, device, pretrained_path)
+    train_distillation_defence(model, device, "/home/ModelRobustnessClassifier/distilled_labels/labels_temp_10.csv")
